31c570e9d9ac203018fd552437382d35b08e0649
Klas Lindfors <klas@yubico.com>
2013-08-15 08:49:26 +0200

    allow .cap as release extension

--- update-releases.py
+++ update-releases.py
@@ -3,7 +3,7 @@
 import os
 from distutils.version import LooseVersion
 
-SUFFIXES = ['tar', 'gz', 'sig', 'tgz', 'zip']
+SUFFIXES = ['tar', 'gz', 'sig', 'tgz', 'zip', 'cap']
 CLASSIFIERS = ['win32', 'win64', 'mac']
 
 RELEASE_TEMPLATE = """
